558bdc246973674f20bac138a4bce81a1cc35fbb
root <root@email>
2023-03-19 17:03:06 +0800

    first commit

---
+++ src/main/java/com/mylch/Mylch1Application.java
@@ -0,0 +1,13 @@
+package com.mylch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mylch1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mylch1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/mylch/Mylch1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mylch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mylch1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
